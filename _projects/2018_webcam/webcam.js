051a6d3087aaf8386f58e66e1d27588099779182
benjaminhabert <bhabert@quantmetry.com>
2018-04-08 12:07:14 +0200

    fix: syntax

--- _projects/2018_webcam/webcam.js
+++ _projects/2018_webcam/webcam.js
@@ -2,12 +2,12 @@ let buffer;
 let spots = [];
 
 function setup() {
-  let canvas = createCanvas(640, 480)
+  let canvas = createCanvas(640, 480);
   canvas.parent('p5sketch');
   colorMode(HSB);
-  ellipseMode(CENTER)
+  ellipseMode(CENTER);
 
-  frameRate(20)
+  frameRate(20);
   buffer = new VideoBuffer(width / 4, height / 4);
   for (let i = 0; i < 3000; i++){
     spots.push(new Spot(canvas, buffer));
@@ -19,7 +19,7 @@ function draw() {
   buffer.update();
 
   // Do the actual drawing
-  background(0)
+  background(0);
   if (frameCount > 10) {
     for (spot of spots) {
       spot.update();
@@ -49,13 +49,13 @@ class Spot {
 
   update() {
     let videoColor = this.buffer.getColor(this.xBuffer, this.yBuffer);
-    let b = brightness(videoColor)
-    this.radius = map(b, 0, 80, this.maxRadius, 1)
+    let b = brightness(videoColor);
+    this.radius = map(b, 0, 80, this.maxRadius, 1);
     this.brightness = map(b, 0, 100, 100, 30);
   }
 
   show() {
-    noStroke()
+    noStroke();
     fill(this.hue, this.saturation, this.brightness);
     ellipse(this.x, this.y, this.radius, this.radius);
   }
@@ -66,7 +66,7 @@ class VideoBuffer {
   constructor(width, height) {
     this.width = width;
     this.height = height;
-    this.capture = createCapture(VIDEO)
+    this.capture = createCapture(VIDEO);
     this.capture.size(this.width, this.height);
     this.buffer = createGraphics(this.width, this.height);
     this.buffer.pixelDensity(1);
@@ -77,47 +77,47 @@ class VideoBuffer {
   }
 
   update() {
-    this.copyWebcamToBuffer()
-    this.buffer.loadPixels()
+    this.copyWebcamToBuffer();
+    this.buffer.loadPixels();
   }
 
   getColor(x, y) {
-    x = int(x)
-    y = int(y)
-    let index = 4 * (x + y * this.width)
+    x = int(x);
+    y = int(y);
+    let index = 4 * (x + y * this.width);
     return this.buffer.color(
       this.buffer.pixels[index],
       this.buffer.pixels[index + 1],
       this.buffer.pixels[index + 2],
-      this.buffer.pixels[index + 3],
-    )
+      this.buffer.pixels[index + 3]
+    );
   }
 
   copyWebcamToBuffer() {
-    this.buffer.push()
+    this.buffer.push();
     this.buffer.translate(this.width, 0);
-    this.buffer.scale(-1.0, 1.0)
+    this.buffer.scale(-1.0, 1.0);
     this.buffer.image(this.capture, 0, 0);
-    this.buffer.pop()
+    this.buffer.pop();
   }
 }
 
 
 // UTILITY FUNCTIONS
 function debug() {
-  push()
+  push();
   image(buffer.getBuffer(), 0, 0);
-  fill(0)
-  text(trackFrameRate(), 10, 15)
-  pop()
+  fill(0);
+  text(trackFrameRate(), 10, 15);
+  pop();
 }
 
 let frameRates = []
 function trackFrameRate() {
-  frameRates.push(frameRate())
+  frameRates.push(frameRate());
   if (frameRates.length > 100){
-    frameRates.shift()
+    frameRates.shift();
   }
   let average = frameRates.reduce((pv, cv) => pv+cv, 0) / frameRates.length;
-  return average.toFixed(2)
+  return average.toFixed(2);
 }
